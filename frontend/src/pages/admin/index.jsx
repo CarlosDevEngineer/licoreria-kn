123e94f99e49dcede74be49b71bb4493f539b355
Carlos Colque <manuel.intern@captivasoft.net>
2026-03-29 12:37:39 -0400

    refactor: organize folder

--- frontend/src/pages/admin/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div>
-      <h1>Panel Admin</h1>
-      <p>Bienvenido administrador</p>
-    </div>
-  );
-}
--- frontend/src/pages/vendedor/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div>
-      <h1>Panel Vendedor</h1>
-      <p>Bienvenido vendedor</p>
-    </div>
-  );
-}
